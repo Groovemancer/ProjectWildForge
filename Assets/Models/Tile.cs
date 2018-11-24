89208f396a6023fafd86144d7e7aba2bfcc53262
Tim Downing <icedmetal57@gmail.com>
2018-11-24 04:45:31 -0600

    Added Bunch o Stuff
    * Added Tiles
    * Added World
    * Added Tile Sprites
    * Added UI Button control
    * Added Camera Panning/Zooming
    * Added build/destroy floor modes with UI support

---
+++ Assets/Models/Tile.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TileType { Dirt, RoughStone, Marsh, ShallowWater, Grass, Floor, Empty };
+
+[Serializable]
+public class Tile
+{
+    TileType _type = TileType.Empty;
+    public TileType Type
+    {
+        get { return _type; }
+        set
+        {
+            TileType oldType = _type;
+            _type = value;
+
+            // Call the callback and let things know we've changed.
+            if (cbTileTypeChanged != null && _type != oldType)
+                cbTileTypeChanged(this);
+        }
+    }
+
+    // The function we callback any time our type changes
+    Action<Tile> cbTileTypeChanged;
+
+    LooseObject looseObject;
+    InstalledObject installedObject;
+
+    World world;
+    public int X { get; protected set; }
+    public int Y { get; protected set; }
+
+    public Tile(World world, int x, int y)
+    {
+        this.world = world;
+        this.X = x;
+        this.Y = y;
+    }
+
+    public void RegisterTileTypeChangedCallback(Action<Tile> callback)
+    {
+        cbTileTypeChanged += callback;
+    }
+
+    public void UnregisterTileTypeChangedCallback(Action<Tile> callback)
+    {
+        cbTileTypeChanged -= callback;
+    }
+}
+
+[Serializable]
+public struct TileSprite
+{
+    [SerializeField]
+    public TileType Type;
+
+    [SerializeField]
+    public Sprite Sprite;
+}
---
+++ Assets/UI/Editor/AutoHorizSizeEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AutoHorizSize))]
+public class AutoHorizSizeEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Recalc Size"))
+        {
+            ((AutoHorizSize)target).AdjustSize();
+        }
+    }
+}
