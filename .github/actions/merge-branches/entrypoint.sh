19cbf28b3a242051a7122a99a2cb8af4418368e2
icedmetal57 <icedmetal57@gmail.com>
2020-06-26 23:27:19 -0500

    Update

--- .github/actions/merge-branches/entrypoint.sh
+++ .github/actions/merge-branches/entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 
 echo
 echo "  'Nightly Merge Action' is using the following input:"
-echo "    - stable_branch = '$INPUT_STABLE_BRANCH'"
-echo "    - development_branch = '$INPUT_DEVELOPMENT_BRANCH'"
+echo "    - source_branch = '$INPUT_SOURCE_BRANCH'"
+echo "    - target_branch = '$INPUT_TARGET_BRANCH'"
 echo "    - allow_ff = $INPUT_ALLOW_FF"
 echo "    - allow_git_lfs = $INPUT_GIT_LFS"
 echo "    - ff_only = $INPUT_FF_ONLY"
@@ -49,26 +49,26 @@ git config --global user.email "$INPUT_USER_EMAIL"
 
 set -o xtrace
 
-git fetch origin $INPUT_STABLE_BRANCH
-git checkout -b $INPUT_STABLE_BRANCH origin/$INPUT_STABLE_BRANCH
+git fetch origin $INPUT_SOURCE_BRANCH
+git checkout -b $INPUT_SOURCE_BRANCH origin/$INPUT_SOURCE_BRANCH
 
-git fetch origin $INPUT_DEVELOPMENT_BRANCH
-git checkout -b $INPUT_DEVELOPMENT_BRANCH origin/$INPUT_DEVELOPMENT_BRANCH
+git fetch origin $INPUT_TARGET_BRANCH
+git checkout -b $INPUT_TARGET_BRANCH origin/$INPUT_TARGET_BRANCH
 
-if git merge-base --is-ancestor $INPUT_STABLE_BRANCH $INPUT_DEVELOPMENT_BRANCH; then
+if git merge-base --is-ancestor $INPUT_SOURCE_BRANCH $INPUT_TARGET_BRANCH; then
   echo "No merge is necessary"
   exit 0
 fi;
 
 set +o xtrace
 echo
-echo "  'Nightly Merge Action' is trying to merge the '$INPUT_STABLE_BRANCH' branch ($(git log -1 --pretty=%H $INPUT_STABLE_BRANCH))"
-echo "  into the '$INPUT_DEVELOPMENT_BRANCH' branch ($(git log -1 --pretty=%H $INPUT_DEVELOPMENT_BRANCH))"
+echo "  'Nightly Merge Action' is trying to merge the '$INPUT_SOURCE_BRANCH' branch ($(git log -1 --pretty=%H $INPUT_SOURCE_BRANCH))"
+echo "  into the '$INPUT_TARGET_BRANCH' branch ($(git log -1 --pretty=%H $INPUT_TARGET_BRANCH))"
 echo
 set -o xtrace
 
 # Do the merge
-git merge $FF_MODE --no-edit $INPUT_STABLE_BRANCH
+git merge $FF_MODE --no-edit $INPUT_SOURCE_BRANCH
 
 # Pull lfs if enabled
 if $INPUT_GIT_LFS; then
@@ -76,4 +76,4 @@ if $INPUT_GIT_LFS; then
 fi
 
 # Push the branch
-git push origin $INPUT_DEVELOPMENT_BRANCH
+git push origin $INPUT_TARGET_BRANCH
