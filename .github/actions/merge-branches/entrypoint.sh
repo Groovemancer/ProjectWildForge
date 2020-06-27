31c22d39b0a9007ef9e937835b3569e8c3e5d94e
icedmetal57 <icedmetal57@gmail.com>
2020-06-26 23:29:47 -0500

    Update Again

--- .github/actions/merge-branches/entrypoint.sh
+++ .github/actions/merge-branches/entrypoint.sh
@@ -49,9 +49,11 @@ git config --global user.email "$INPUT_USER_EMAIL"
 
 set -o xtrace
 
+echo "Checkout source"
 git fetch origin $INPUT_SOURCE_BRANCH
 git checkout -b $INPUT_SOURCE_BRANCH origin/$INPUT_SOURCE_BRANCH
 
+echo "Checkout target"
 git fetch origin $INPUT_TARGET_BRANCH
 git checkout -b $INPUT_TARGET_BRANCH origin/$INPUT_TARGET_BRANCH
 
